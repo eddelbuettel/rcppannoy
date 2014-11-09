70c57ed85b5527491f3dc9d3507f423d417cbcb2
Dirk Eddelbuettel <edd@debian.org>
2014-11-08 19:41:56 -0600

    updated for templated version

--- demo/simpleExample.R
+++ demo/simpleExample.R
@@ -6,20 +6,19 @@ library(RcppAnnoy)
 set.seed(123)                           # be reproducible
 
 f <- 40
-a <- new(Annoy, f)
-
-n <- 150                                # not specified
+a <- new(AnnoyEuclidean, f)
+n <- 5                                  # not specified
 
 for (i in seq(n)) {
     v <- rnorm(f)
     a$addItem(i-1, v)
 }
 
-a$build(n)                           	# 50 trees
+a$build(50)                           	# 50 trees
 a$save("/tmp/test.tree")
 
 
-b <- new(Annoy, f)               	# new object, could be in another process
+b <- new(AnnoyEuclidean, f)           	# new object, could be in another process
 b$load("/tmp/test.tree")		# super fast, will just mmap the file
 
 print(b$getNNsByItem(0, 1000))
