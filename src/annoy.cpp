f3e9caf6425f6a0672d35e65b2ffb44c2951c01b
Dirk Eddelbuettel <edd@debian.org>
2014-11-12 06:44:37 -0600

    added copyright and license header

--- src/annoy.cpp
+++ src/annoy.cpp
@@ -1,4 +1,30 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+//  RcppAnnoy -- Rcpp bindings to Annoy library for Approximate Nearest Neighbours
+//
+//  Copyright (C) 2014           Dirk Eddelbuettel 
+//
+//  This file is part of RcppAnnoy
+//
+//  RcppAnnoy is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  RcppAnnoy is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with RcppAnnoy.  If not, see <http://www.gnu.org/licenses/>.
+
+// simple C++ modules to wrap to two templated classes from Annoy
+//
+// uses annoylib.h (from Annoy) and provides R access via Rcpp
+//
+// Dirk Eddelbuettel, Nov 2014
+
 
 #include <Rcpp.h>
 
