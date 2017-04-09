fd30cf7c8e63847e9b344a3974f61b6115e41bb2
Dirk Eddelbuettel <edd@debian.org>
2017-04-09 14:43:04 -0500

    added registration

---
+++ src/init.c
@@ -0,0 +1,23 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Call calls */
+extern SEXP _rcpp_module_boot_AnnoyAngular();
+extern SEXP _rcpp_module_boot_AnnoyEuclidean();
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_rcpp_module_boot_AnnoyAngular", (DL_FUNC) &_rcpp_module_boot_AnnoyAngular, 0},
+    {"_rcpp_module_boot_AnnoyEuclidean", (DL_FUNC) &_rcpp_module_boot_AnnoyEuclidean, 0},
+    {NULL, NULL, 0}
+};
+
+void R_init_RcppAnnoy(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
