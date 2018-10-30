b4dafb2538e9cfc867fb06011047f246ab6f1d05
Dirk Eddelbuettel <edd@debian.org>
2018-10-29 19:43:26 -0500

    updated windows #define and simplified vignette setup

--- inst/include/annoylib.h
+++ inst/include/annoylib.h
@@ -35,12 +35,14 @@ typedef unsigned __int64  uint64_t;
 #include <stdint.h>
 #endif
 
-#ifdef _MSC_VER
-#define NOMINMAX
-#include "mman.h"
-#include <windows.h>
+#ifdef __MINGW32__
+ #ifndef NOMINMAX
+  #define NOMINMAX
+ #endif
+ #include "mman.h"
+ #include <windows.h>
 #else
-#include <sys/mman.h>
+ #include <sys/mman.h>
 #endif
 
 #include <string.h>
