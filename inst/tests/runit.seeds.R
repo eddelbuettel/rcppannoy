d14711314e5a8411d76b435493206c8b32e36c1a
Dirk Eddelbuettel <edd@debian.org>
2019-04-11 17:23:10 -0500

    rewrote new test

--- inst/tests/runit.seeds.R
+++ inst/tests/runit.seeds.R
@@ -11,25 +11,31 @@ if (.runThisTest) {
     test01seeds <- function() {
         f <- 2
 
-        a <- new(AnnoyHamming, f)
-        a$setSeed(123)
-        a$addItem(0, c(0, 1))
-        a$addItem(1, c(1, 1))
-        a$addItem(2, c(0, 0))
+        set.seed(123456)   # R Seed for next two vectors
+        n <- 100
+        x <- rnorm(n)
+        y <- rnorm(n)
 
-        b <- new(AnnoyHamming, f)
-        b$setSeed(456)
-        b$addItem(0, c(0, 1))
-        b$addItem(1, c(1, 1))
-        b$addItem(2, c(0, 0))
+        v1 <- new(AnnoyHamming, f)
+        v1$setSeed(123)
+        for (i in 1:n) v1$addItem(i-1, c(x[i], y[i]))
+        v1$build(f)
 
-        c <- new(AnnoyHamming, f)
-        c$setSeed(456)
-        c$addItem(0, c(0, 1))
-        c$addItem(1, c(1, 1))
-        c$addItem(2, c(0, 0))
+        v2 <- new(AnnoyHamming, f)
+        v2$setSeed(456)  # different
+        for (i in 1:n) v2$addItem(i-1, c(x[i], y[i]))
+        v2$build(f)
 
-        checkEquals(a$getDistance(0, 1), c$getDistance(0, 1), msg="a and c are equal")
-        checkTrue(a$getDistance(0, 1) != b$getDistance(0, 1), msg="a abd b not equal")
+        v3 <- new(AnnoyHamming, f)
+        v3$setSeed(123)  # as first
+        for (i in 1:n) v3$addItem(i-1, c(x[i], y[i]))
+        v3$build(f)
+
+        checkEquals(v1$getNNsByVector(c(0.5,0.5), 20),
+                    v3$getNNsByVector(c(0.5,0.5), 20),
+                    msg="v1 and v3 are equal")
+        checkTrue(sum(abs(v1$getNNsByVector(c(0.5,0.5), 20) -
+                          v2$getNNsByVector(c(0.5,0.5), 20))) > 0,
+                  msg="v1 and v2 are not equal")
     }
 }
