ae4b3cd6fc7dc611c2fbb2f56dca39cdf89d5035
Dirk Eddelbuettel <edd@debian.org>
2020-03-03 05:51:19 -0600

    add helper displaying AVX (and compiler) status

---
+++ src/arch.cpp
@@ -0,0 +1,34 @@
+
+#include <annoylib.h>
+#include <Rcpp.h>
+
+#if defined(USE_AVX512)
+#define AVX_INFO "Using 512-bit AVX instructions"
+#elif defined(USE_AVX128)
+#define AVX_INFO "Using 128-bit AVX instructions"
+#else
+#define AVX_INFO "Not using AVX instructions"
+#endif
+
+#if defined(_MSC_VER)
+#define COMPILER_INFO "Compiled using MSC"
+#elif defined(__GNUC__)
+#define COMPILER_INFO "Compiled using GCC"
+#elif defined(__clang__)
+#define COMPILER_INFO "Compiled using Clang"
+#else
+#define COMPILER_INFO "Compiled on unknown platform"
+#endif
+
+#define ANNOY_DOC (COMPILER_INFO ". " AVX_INFO ".")
+
+//' Report CPU Architecture and Compiler
+//'
+//' @return A constant direct created at compile-time describing
+//' the extent of AVX instructions (512 bit, 128 bit, or none)
+//' and compiler use where currently recognised are MSC (unlikely
+//' for R), GCC, Clang, or \sQuote{other}.
+// [[Rcpp::export]]
+std::string getArchictectureStatus() {
+  return std::string(ANNOY_DOC);
+}
--- src/init.c
+++ src/init.c
@@ -12,12 +12,14 @@ extern SEXP _rcpp_module_boot_AnnoyAngular();
 extern SEXP _rcpp_module_boot_AnnoyEuclidean();
 extern SEXP _rcpp_module_boot_AnnoyManhattan();
 extern SEXP _rcpp_module_boot_AnnoyHamming();
+extern SEXP _RcppAnnoy_getArchictectureStatus();
 
 static const R_CallMethodDef CallEntries[] = {
     {"_rcpp_module_boot_AnnoyAngular",   (DL_FUNC) &_rcpp_module_boot_AnnoyAngular,   0},
     {"_rcpp_module_boot_AnnoyEuclidean", (DL_FUNC) &_rcpp_module_boot_AnnoyEuclidean, 0},
     {"_rcpp_module_boot_AnnoyManhattan", (DL_FUNC) &_rcpp_module_boot_AnnoyManhattan, 0},
     {"_rcpp_module_boot_AnnoyHamming",   (DL_FUNC) &_rcpp_module_boot_AnnoyHamming,   0},
+    {"_RcppAnnoy_getArchictectureStatus",(DL_FUNC) &_RcppAnnoy_getArchictectureStatus,0},
     {NULL, NULL, 0}
 };
 
