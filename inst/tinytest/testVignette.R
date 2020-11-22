3e4ea952cfbe751d41d118720cce8a337d35e6ea
Dirk Eddelbuettel <edd@debian.org>
2020-11-22 08:37:11 -0600

    make test operational by moving Rmd source, add tempfile() use

--- inst/tinytest/testVignette.R
+++ inst/tinytest/testVignette.R
@@ -1,7 +1,10 @@
 
-## See #66 for the initial idea
+## See #66 for the idea and discussion
 
-lines <- readLines("../../vignettes/rmd/UsingAnnoyInCpp.Rmd")
+vigfile <- system.file("rmd", "UsingAnnoyInCpp.Rmd", package="RcppAnnoy")
+if (!file.exists(vigfile)) exit_file("No vignette source found. What's up with that?")
+
+lines <- readLines(vigfile)
 starts <- which(lines == "```{Rcpp, eval=FALSE}")
 ends <- which(lines=="```")
 ends <- ends[findInterval(starts, ends)+1]
@@ -10,13 +13,9 @@ res <- Rcpp::sourceCpp(code=paste(code, collapse="\n")) # checks everything is c
 
 expect_equal(res$functions, "thingy")   # check we got a function compiled
 
-wd <- getwd()
-setwd(tempdir())
-
 set.seed(42)
 mat <- matrix(runif(1000), 100)
 Q <- matrix(runif(100), 10)
-res <- thingy(mat, 1, 10, Q)
+res <- thingy(mat, 1, 10, Q, tempfile())
 expect_equal(res, 1)
 
-setwd(wd)
