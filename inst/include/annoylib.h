2fb60b45f74ce1b994cbe75639b4ea9f0aba53e9
Dirk Eddelbuettel <edd@debian.org>
2022-10-27 08:53:37 -0500

    Update to appease both clang-15 and xcode/macos 14

--- inst/include/annoylib.h
+++ inst/include/annoylib.h
@@ -95,7 +95,7 @@ inline void set_error_from_errno(char **error, const char* msg) {
   showUpdate("%s: %s (%d)\n", msg, strerror(errno), errno);
   if (error) {
     *error = (char *)malloc(256);  // TODO: win doesn't support snprintf
-    sprintf(*error, "%s: %s (%d)", msg, strerror(errno), errno);
+    snprintf(*error, 255, "%s: %s (%d)", msg, strerror(errno), errno);
   }
 }
 
