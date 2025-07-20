092a1414d855a41501db8bfbd7f33897afd73d29
Dirk Eddelbuettel <edd@debian.org>
2025-07-20 11:33:05 -0500

    For the time being switch to R.rsp::asis

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,15 +1,4 @@
-
 if (requireNamespace("tinytest", quietly=TRUE)) {
-
-    ## Set a seed to make the test deterministic
-    set.seed(42)
-
-    ## there are several more granular ways to test files in a
-    ## tinytest directory, see its package vignette; tests can also
-    ## run once the package is installed using the same command
-
-    ## we need version 0.9.3 or later
-    if (packageVersion("tinytest") >= "0.9.3") {
-        tinytest::test_package("RcppAnnoy")
-    }
+    set.seed(42) 	# Set a seed to make the test deterministic
+    tinytest::test_package("RcppAnnoy")
 }
